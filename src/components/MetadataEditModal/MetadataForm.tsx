22f6da7440a30f0219ec536cf0479a4ae538628e
lyswhut <lyswhut@qq.com>
2025-08-10 13:46:12 +0800

    新增 Any Listen 歌词标签数据生成与读取播放

--- src/components/MetadataEditModal/MetadataForm.tsx
+++ src/components/MetadataEditModal/MetadataForm.tsx
@@ -11,6 +11,7 @@ import { downloadFile, mkdir, stat } from '@/utils/fs'
 import { useUnmounted } from '@/utils/hooks'
 import { getLyricInfo, getPicUrl } from '@/core/music/local'
 import settingState from '@/store/setting/state'
+import { buildLyrics } from '@/utils/lrcTools'
 
 export interface Metadata {
   name: string // 歌曲名
@@ -150,26 +151,18 @@ export default forwardRef<MetadataFormType, {}>((props, ref) => {
         source: 'local',
       },
       isRefresh: false,
+    }).then(async(lrcData) => {
+      if (isUnmounted.current || path != filePath.current) return
+      toast(t('metadata_edit_modal_form_match_lyric_success'))
+      setData(data => {
+        return { ...data, lyric: buildLyrics(lrcData, true, settingState.setting['player.isShowLyricTranslation'], settingState.setting['player.isShowLyricRoma']) }
+      })
+    }).catch(() => {
+      if (isUnmounted.current || path != filePath.current) return
+      toast(t('metadata_edit_modal_form_match_lyric_failed'))
+    }).finally(() => {
+      matcheingLrc.delete(path)
     })
-      .then(async ({ lyric, tlyric, rlyric }) => {
-        if (isUnmounted.current || path != filePath.current) return
-        toast(t('metadata_edit_modal_form_match_lyric_success'))
-        let lrc = [
-          lyric,
-          settingState.setting['player.isShowLyricTranslation'] && tlyric ? tlyric : '',
-          settingState.setting['player.isShowLyricRoma'] && rlyric ? rlyric : '',
-        ]
-        setData((data) => {
-          return { ...data, lyric: lrc.join('\n\n').trim() }
-        })
-      })
-      .catch(() => {
-        if (isUnmounted.current || path != filePath.current) return
-        toast(t('metadata_edit_modal_form_match_lyric_failed'))
-      })
-      .finally(() => {
-        matcheingLrc.delete(path)
-      })
   }, [data.albumName, data.name, data.singer, t])
   const handleUpdatePic = useCallback((path: string) => {
     setData((data) => {
