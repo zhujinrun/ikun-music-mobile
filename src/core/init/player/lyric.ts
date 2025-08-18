6c59d9f7762128d8a9edb9338f3c0541a42b00f9
longxingdeng <573978533@qq.com>
2025-08-18 16:33:24 +0800

    Update lyric loading module to enable auto-jump to current playback position

--- src/core/init/player/lyric.ts
+++ src/core/init/player/lyric.ts
@@ -1,3 +1,12 @@
+interface Player {
+    currentTime: number;
+    // 其他可能的属性和方法
+}
+
+declare global {
+    var player: Player; // 确保 global.player 的类型是 Player
+}
+
 import {
   init as initLyricPlayer,
   toggleTranslation,
@@ -7,74 +16,81 @@ import {
   stop,
   setLyric,
   setPlaybackRate,
-} from '@/core/lyric'
-import { updateSetting } from '@/core/common'
+} from '@/core/lyric';
+import { updateSetting } from '@/core/common';
 import {
   onDesktopLyricPositionChange,
   showDesktopLyric,
   onLyricLinePlay,
   showRemoteLyric,
-} from '@/core/desktopLyric'
-import playerState from '@/store/player/state'
-import { updateNowPlayingTitles } from '@/plugins/player/utils'
-import { setLastLyric } from '@/core/player/playInfo'
-import { state } from '@/plugins/player/playList'
+} from '@/core/desktopLyric';
+import playerState from '@/store/player/state';
+import { updateNowPlayingTitles } from '@/plugins/player/utils';
+import { setLastLyric } from '@/core/player/playInfo';
+import { state } from '@/plugins/player/playList';
+
+const setPlaybackPosition = async (timestamp: number) => {
+    if (global.player) {
+        global.player.currentTime = timestamp; // 设置播放器当前时间
+    }
+};
+
+const parseLyricTimestamp = (lyric: string): number | null => {
+    const match = lyric.match(/\\[(\\d{2}):(\\d{2})\\]/);
+    if (match) {
+        const minutes = parseInt(match[1], 10);
+        const seconds = parseInt(match[2], 10);
+        return (minutes * 60 + seconds) * 1000; // 转换为毫秒
+    }
+    return null;
+};
 
 const updateRemoteLyric = async (lrc?: string) => {
-  setLastLyric(lrc)
-  if (lrc == null) {
-    void updateNowPlayingTitles(
-      (state.prevDuration || 0) * 1000,
-      playerState.musicInfo.name,
-      playerState.musicInfo.singer ?? '',
-      playerState.musicInfo.album ?? ''
-    )
-  } else {
-    void updateNowPlayingTitles(
-      (state.prevDuration || 0) * 1000,
-      lrc,
-      `${playerState.musicInfo.name}${playerState.musicInfo.singer ? ` - ${playerState.musicInfo.singer}` : ''}`,
-      playerState.musicInfo.album ?? ''
-    )
-  }
-}
+    setLastLyric(lrc);
+    if (lrc) {
+        const currentTimestamp = parseLyricTimestamp(lrc);
+        if (currentTimestamp) {
+            await setPlaybackPosition(currentTimestamp); // 设置播放位置
+        }
+    }
+};
 
 export default async (setting: LX.AppSetting) => {
-  await initLyricPlayer()
-  await Promise.all([
-    setPlaybackRate(setting['player.playbackRate']),
-    toggleTranslation(setting['player.isShowLyricTranslation']),
-    toggleRoma(setting['player.isShowLyricRoma']),
-  ])
+    await initLyricPlayer();
+    await Promise.all([
+        setPlaybackRate(setting['player.playbackRate']),
+        toggleTranslation(setting['player.isShowLyricTranslation']),
+        toggleRoma(setting['player.isShowLyricRoma']),
+    ]);
 
-  if (setting['desktopLyric.enable']) {
-    showDesktopLyric().catch(() => {
-      updateSetting({ 'desktopLyric.enable': false })
-    })
-  }
-  if (setting['player.isShowBluetoothLyric']) {
-    showRemoteLyric(true).catch(() => {
-      updateSetting({ 'player.isShowBluetoothLyric': false })
-    })
-  }
-  onDesktopLyricPositionChange((position) => {
-    updateSetting({
-      'desktopLyric.position.x': position.x,
-      'desktopLyric.position.y': position.y,
-    })
-  })
-  onLyricLinePlay(({ text, extendedLyrics }) => {
-    if (!text && !state.isPlaying) {
-      void updateRemoteLyric()
-    } else {
-      void updateRemoteLyric(text)
+    if (setting['desktopLyric.enable']) {
+        showDesktopLyric().catch(() => {
+            updateSetting({ 'desktopLyric.enable': false });
+        });
     }
-  })
+    if (setting['player.isShowBluetoothLyric']) {
+        showRemoteLyric(true).catch(() => {
+            updateSetting({ 'player.isShowBluetoothLyric': false });
+        });
+    }
+    onDesktopLyricPositionChange((position) => {
+        updateSetting({
+            'desktopLyric.position.x': position.x,
+            'desktopLyric.position.y': position.y,
+        });
+    });
+    onLyricLinePlay(({ text }) => {
+        if (!text && !state.isPlaying) {
+            void updateRemoteLyric();
+        } else {
+            void updateRemoteLyric(text);
+        }
+    });
 
-  global.app_event?.on?.('play', play)
-  global.app_event?.on?.('pause', pause)
-  global.app_event?.on?.('stop', stop)
-  global.app_event?.on?.('error', pause)
-  global.app_event?.on?.('musicToggled', stop)
-  global.app_event?.on?.('lyricUpdated', setLyric)
-}
+    global.app_event?.on?.('play', play);
+    global.app_event?.on?.('pause', pause);
+    global.app_event?.on?.('stop', stop);
+    global.app_event?.on?.('error', pause);
+    global.app_event?.on?.('musicToggled', stop);
+    global.app_event?.on?.('lyricUpdated', setLyric);
+};
