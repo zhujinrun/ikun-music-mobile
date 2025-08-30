96bdf112068903f2963174d9bcc9e9e92e2ecb14
lyswhut <lyswhut@qq.com>
2025-08-30 16:03:52 +0800

    更新依赖，更新播放器组件

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,15 +1,31 @@
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
+import createIconSet from '@react-native-vector-icons/icomoon'
 import icoMoonConfig from '@/resources/fonts/selection.json'
 import { scaleSizeW } from '@/utils/pixelRatio'
 import { memo, type ComponentProps } from 'react'
 import { useTextShadow, useTheme } from '@/store/theme/hook'
 import { StyleSheet, type StyleProp, type TextStyle } from 'react-native'
 
-const IcoMoon = createIconSetFromIcoMoon(icoMoonConfig)
+// import IconAntDesign from 'react-native-vector-icons/AntDesign'
+// import IconEntypo from 'react-native-vector-icons/Entypo'
+// import IconEvilIcons from 'react-native-vector-icons/EvilIcons'
+// import IconFeather from 'react-native-vector-icons/Feather'
+// import IconFontAwesome from 'react-native-vector-icons/FontAwesome'
+// import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+// import IconFontisto from 'react-native-vector-icons/Fontisto'
+// import IconFoundation from 'react-native-vector-icons/Foundation'
+// import IconIonicons from 'react-native-vector-icons/Ionicons'
+// import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons'
+// import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+// import IconOcticons from 'react-native-vector-icons/Octicons'
+// import IconZocial from 'react-native-vector-icons/Zocial'
+// import IconSimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+
+
+const IcoMoon = createIconSet(icoMoonConfig)
 
 // https://oblador.github.io/react-native-vector-icons/
 
-type IconType = ReturnType<typeof createIconSetFromIcoMoon>
+type IconType = ReturnType<typeof createIconSet>
 
 interface IconProps extends Omit<ComponentProps<IconType>, 'style'> {
   style?: StyleProp<TextStyle>
@@ -33,7 +49,6 @@ export const Icon = memo(({ size = 15, rawSize, color, style, ...props }: IconPr
     <IcoMoon
       size={rawSize ?? scaleSizeW(size)}
       color={color ?? theme['c-font']}
-      // @ts-expect-error
       style={newStyle}
       {...props}
     />
