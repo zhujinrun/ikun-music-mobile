222c7eedc0df8b8a662bdee87c2636544132bfca
ikun0014 <ikun0014@qq.com>
2025-11-28 22:32:10 +0800

    更新版本号至1.8.6
    升级package.json和version.json中的版本号至1.8.6，versionCode至75，为后续功能或修复做准备。

--- src/utils/version.js
+++ src/utils/version.js
@@ -7,17 +7,10 @@ import { APP_PROVIDER_NAME } from '@/config/constant'
 const abis = ['arm64-v8a', 'armeabi-v7a', 'x86_64', 'x86', 'universal']
 
 const address = [
-  [
-    `https://raw.githubusercontent.com/${author.name}/${name}/master/publish/version.json`,
-    'direct',
-  ],
-  ['https://registry.npmjs.org/lx-music-mobile-version-info/latest', 'npm'],
+  [`https://raw.gitmirror.com/${author.name}/${name}/master/publish/version.json`, 'direct'],
   [`https://cdn.jsdelivr.net/gh/${author.name}/${name}/publish/version.json`, 'direct'],
   [`https://fastly.jsdelivr.net/gh/${author.name}/${name}/publish/version.json`, 'direct'],
   [`https://gcore.jsdelivr.net/gh/${author.name}/${name}/publish/version.json`, 'direct'],
-  ['https://registry.npmmirror.com/lx-music-mobile-version-info/latest', 'npm'],
-  ['https://gitee.com/lyswhut/lx-music-mobile-versions/raw/master/version.json', 'direct'],
-  ['http://cdn.stsky.cn/lx-music/mobile/version.json', 'direct'],
 ]
 
 const request = async (url, retryNum = 0) => {
@@ -86,7 +79,7 @@ let apkSavePath
 
 export const downloadNewVersion = async (version, onDownload = noop) => {
   const abi = await getTargetAbi()
-  const url = `https://github.com/${author.name}/${name}/releases/download/v${version}/${name}-v${version}-${abi}.apk`
+  const url = `https://gh-proxy.org/https://github.com/${author.name}/${name}/releases/download/v${version}/${name}-v${version}-${abi}.apk`
   let savePath = temporaryDirectoryPath + '/lx-music-mobile.apk'
 
   if (downloadJobId) stopDownload(downloadJobId)
@@ -97,14 +90,6 @@ export const downloadNewVersion = async (version, onDownload = noop) => {
     readTimeout: 30000,
     begin({ statusCode, contentLength }) {
       onDownload(contentLength, 0)
-      // switch (statusCode) {
-      //   case 200:
-      //   case 206:
-      //     break
-      //   default:
-      //     onDownload(null, contentLength, 0)
-      //     break
-      // }
     },
     progress({ contentLength, bytesWritten }) {
       onDownload(contentLength, bytesWritten)
